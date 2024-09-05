1c66663e817bfb3d8ea60b385cce44a71a4a4332
Jeroimo <jeronimoriveros@gmail.com>
2024-09-05 11:45:45 -0500

    updated CV links

--- portafolioproyect/src/components/portafolio.jsx
+++ portafolioproyect/src/components/portafolio.jsx
@@ -76,7 +76,7 @@ const Portafolio = ({ language, darkMode, toggleDarkMode }) => {
 
 </svg>
 </span></button>
-              <a href="https://drive.google.com/file/d/1umPmSz62mq-bcXOoYK397h3dXubwrb-H/view?usp=sharing" target='_blank'><span className="icon_download">
+              <a href="https://drive.google.com/file/d/1hf0yYf3hkurcMUsJNodRmKFDWQm6Savl/view" target='_blank'><span className="icon_download">
               <svg fill="#000000" width="30px" height="30px" viewBox="-1.6 -1.6 19.20 19.20" id="download-cloud-16px" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00016" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)">
 
 <g id="SVGRepo_bgCarrier" stroke-width="0"/>
@@ -336,7 +336,7 @@ const Portafolio = ({ language, darkMode, toggleDarkMode }) => {
 <path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </span></button>
-              <a className="ligth_Mode" href="https://drive.google.com/file/d/1umPmSz62mq-bcXOoYK397h3dXubwrb-H/view?usp=sharing" target='_blank'><span className="icon_download">
+              <a className="ligth_Mode" href="https://drive.google.com/file/d/1hf0yYf3hkurcMUsJNodRmKFDWQm6Savl/view" target='_blank'><span className="icon_download">
               <svg fill="white" width="30px" height="30px" viewBox="-1.6 -1.6 19.20 19.20" id="download-cloud-16px" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.00016" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)">
 
 <g id="SVGRepo_bgCarrier" stroke-width="0"/>
