66d3276e08aa470374663cfd2ad45be5801cb380
Jeroimo <jeronimoriveros@gmail.com>
2025-01-07 00:05:28 -0500

    work experience updated

--- portafolioproyect/src/components/Work/WorkExpericeSection.jsx
+++ portafolioproyect/src/components/Work/WorkExpericeSection.jsx
@@ -4,32 +4,7 @@ import "./workexperiencesection.css";
 const WorkExperienceSection = ({ darkMode = false }) => {
   const { t } = useTranslation("workExperience");
 
-  const workExperiences = [
-    {
-      company: "WMT (CallWave)",
-      role: t("workExperience.wmt.role"),
-      period: t("workExperience.wmt.period"),
-      imageUrl:
-        "https://www.callwave.com/hs-fs/hubfs/CallWave-Homepage-Hero-1.png?width=1920&height=700&name=CallWave-Homepage-Hero-1.png",
-      altText: "WMT Logo",
-      achievements: [
-        t("workExperience.wmt.achievements.first"),
-        t("workExperience.wmt.achievements.second"),
-      ],
-    },
-    {
-      company: "MyWacc",
-      role: t("workExperience.mywacc.role"),
-      period: t("workExperience.mywacc.period"),
-      imageUrl: "https://mywacc.com/images/new-design/home/new-home.png",
-      altText: "MyWacc Logo",
-      achievements: [
-        t("workExperience.mywacc.achievements.first"),
-        t("workExperience.mywacc.achievements.second"),
-        t("workExperience.mywacc.achievements.third"),
-      ],
-    },
-  ];
+  const workExperiences = t("work", { returnObjects: true });
 
   return (
     <section className="work-experience-section" id="work-experience">
